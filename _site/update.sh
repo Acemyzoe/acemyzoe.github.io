531880203a8b4c61a61b00519d9fc5af2310bd02
acemyzoe <acemyzoe@github.com>
2020-08-15 20:27:23 +0800

    20200815

--- _site/update.sh
+++ _site/update.sh
@@ -5,7 +5,6 @@ bundle install
 bundle exec jekyll build
 # bundle exec jekyll serve
 git add .
-ls_date=`date +%Y%m%d`
 #git commit -m "upload blog"
-git commit -m ls_date
+git commit -m $(date "+%Y%m%d")
 git push origin master
